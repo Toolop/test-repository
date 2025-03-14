dceae500294745445b146dfdefc62fc71031a50e
rafi.arya <rafi.arya@sucofindo.co.id>
2025-03-14 14:10:47 +0700

    adding

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
-
-
 //click yes btn
 function clickYesButton() {
-    alert('I love you too <3');
+  alert(
+    "Akuu sayang kamu bangett ayangg :(, kita beli hari selasa ya, maaf sayanggg"
+  );
 }
 
 //move button on hover
 function hoverNoButton() {
-    // random from 0 to 1, then multiply with screen size
-    let x = Math.random() * window.innerWidth;
-    let y = Math.random() * window.innerHeight;
+  // random from 0 to 1, then multiply with screen size
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
 
-    document.getElementById('no-button').style.left = x + 'px';
-    document.getElementById('no-button').style.top = y +'px';
-    
+  document.getElementById("no-button").style.left = x + "px";
+  document.getElementById("no-button").style.top = y + "px";
 }
